2400507c2e762dfbbe6e03754e410f178f915fd0
rebelice <yangrebelice@gmail.com>
2024-06-12 15:21:32 +0800

    fix/fix-merge-statement

--- examples/merge01.sql
+++ examples/merge01.sql
@@ -5,5 +5,13 @@ merge into bonuses d
      delete where (salary > 8000)
    when not matched then insert (d.employee_id, d.bonus) 
      values (s.employee_id, s.salary)
-     where (s.salary <= 8000)
+     where (s.salary <= 8000);
 
+merge into (select * from bonuses) d
+  using (select employee_id.* from employees) s 
+   on (employee_id = a) 
+   when matched then update set d.bonus = bonus 
+     delete where (salary > 8000)
+   when not matched then insert (d.employee_id, d.bonus) 
+     values (s.employee_id, s.salary)
+     where (s.salary <= 8000);
